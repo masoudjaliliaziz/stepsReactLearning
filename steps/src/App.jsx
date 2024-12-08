4ee55eeaeaf05b0e5849218921026424e956922f
masoud_jalili <masoudjalili0781@gmail.com>
2024-12-08 22:59:32 +0330

    setup vite&tailwind

---
+++ steps/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-green-600 text-white font-bold text-2xl p-3 rounded-md">
+      hello React!
+    </div>
+  );
+}
+
+export default App;
